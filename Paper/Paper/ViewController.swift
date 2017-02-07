ff4d5288dd42a86a15baf292f3232f71a7fcdc34
Sangjun Park <vivid950@hanmail.net>
2017-02-07 14:37:13 +0900

    UI대량수정(메인)
    UI대량수정(메인)

--- Paper/Paper/ViewController.swift
+++ Paper/Paper/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var categoryView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func categoryChange(_ sender: Any) {
+        categoryView.willMove(toSuperview:UIView)
+    }
 
 }
 
